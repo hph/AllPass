389b968b84b81ff2677b26c13117c06d49b303c3
Haukur Páll Hallvarðsson <haukur@outoftheblue.is>
2012-01-19 17:59:47 +0000

    Many changes, not tested

--- AllPass.py
+++ AllPass.py
@@ -1,42 +1,43 @@
 #!/usr/bin/env python
 #coding=utf8
 
-# TODO
-# pwd_strength()
-# import optparser
-# import setupdrasl
-# setup shit..
-# ...
-
-import random
-import string
+import hashlib # TODO
+import optparser # TODO
 from getpass import getpass
 from gtk import clipboard_get
+from random import choice, sample
+from string import lowercase, uppercase, digits, punctuation
 from sys import argv
 
-def password(seed, lowercase=4, uppercase=4, digits=4, symbols=4):
-    ''''''
-    rand_str = lambda len: ''.join(sample(letters + digits + punctuation, len))
-    in_chars = lambda chars: any([1 for x in xrange(1,10) if str(x) in chars])
-    random.seed(seed)
-    password = rand_str(16)
-    return password if in_chars( # breyta in_chars, check for any for all.
+CHR_SET = [lowercase, uppercase, digits, punctuation]
 
-def generate_password(seed, lowercase=7, uppercase=3, digits=3, symbols=3):
-    '''Generates and returns a password based upon an input seed.'''
-    # NOTE A simpler and yet acceptable way to do this would be
-    # ''.join(sample(lowercase + uppercase + digits + punctuation, 16))
-    random.seed(seed)
-    password = ''
-    for _ in xrange(lowercase):
-        password += random.choice(string.lowercase)
-    for _ in xrange(uppercase):
-        password += random.choice(string.uppercase)
-    for _ in xrange(digits):
-        password += random.choice(string.digits)
-    for _ in xrange(symbols):
-        password += random.choice(string.punctuation)
-    return ''.join(random.sample(password, len(password)))
+def random_string(length):
+    '''Return a random string of lenght 'len'.'''
+    return ''.join(choice(''.join(CHR_SET)) for _ in xrange(len))
+
+def strong_password(password):
+    '''Return True if 'password' contains at least one lowercase character, one
+    uppercase character, one digit and one symbol. Otherwise return False.'''
+    in_chrs = lambda chrs: any(True for chr in password if chr in chrs)
+    # Check if there's at least one character from each character subset.
+    return all(in_chrs(chr_subset) for chr_subset in CHR_SET)
+
+def password(seed, lower=False, upper=False, digits=False, punct=False):
+    '''Generate and return a password based on 'seed'.'''
+    seed(seed)
+    if any([lower, upper, digits, punct]):
+        # NOTE This is still not good enough.
+        password = ''
+        for arg in [lower, upper, digits, punct]:
+            password += ''.join(choice(chrs) for _ in xrange(arg) for chrs in
+                                CHR_SET)
+        password = ''.join(sample(password, len(password)))
+    else:
+        while True:
+            password = random_string(16)
+            if strong_password(password):
+                break
+    return password
 
 def set_clipboard(text):
     '''Sets text as the clipboard.'''
@@ -45,8 +46,7 @@ def set_clipboard(text):
     clipboard.store()
 
 def main():
-    # NOTE Add more options and improve error handling.
-    # TODO Add more features.
+    # TODO Add set_clipboard to this.
     try:
         print 'AllPass - Your passwords everywhere and nowhere.'
         if '-o' in argv:
@@ -62,14 +62,19 @@ def main():
             uppercase = int(raw_input('Number of uppercase characters: '))
             digits = int(raw_input('Number of digits: '))
             symbols = int(raw_input('Number of symbols: '))
-            generate_password(seed, lowercase, uppercase, digits, symbols)
+            password = password(seed, lowercase, uppercase, digits, symbols)
+            set_clipboard(password)
         else:
-            generate_password((getpass('Seed #1: '), getpass('Seed #2: ')))
+            password = password((getpass('Seed #1: '), getpass('Seed #2: ')))
+            set_clipboard(password)
         print 'Password saved to the clipboard.',
         raw_input('Press enter to clear it and exit.')
         set_clipboard('')
     except:
         print 'Error!'
+        try:
+            if clipboard.get() == password:
+                set.clipboard('')
 
 if __name__ == '__main__':
     main()
