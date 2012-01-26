ca6684604b470be39bb0ed6456b5764caad0ae10
Haukur Páll Hallvarðsson <haukur@outoftheblue.is>
2012-01-26 17:27:14 +0000

    Not necessary, not complete

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 #!/usr/env python
 #coding=utf8
 
+# NOTE This is not to be used as it's not complete.
+from sys import exit
+exit()
+# TODO Finish, maybe?
+
 from sys import argv
 from distutils.core import setup
 
